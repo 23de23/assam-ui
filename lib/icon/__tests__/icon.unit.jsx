9bab770590151df353a892fad931aa0ba6e4dd9b
st <st@lqkj.com>
2021-11-12 17:38:48 +0800

    事件测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,18 @@
 import * as renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../icon'
+import {mount} from 'enzyme'
 
 describe('icon',()=>{
-  it('xxx',()=>{
-    const json = renderer.create(<Icon/>).toJSON()
+  it('render successfully',()=>{
+    const json = renderer.create(<Icon name="alipay"/>).toJSON()
     expect(json).toMatchSnapshot()
+  })
+
+  it('onClick',()=>{
+    const fn = jest.fn()
+    const component = mount(<Icon name='alipay' onClick={fn}/>)
+    component.find('svg').simulate('click') //找到svg模拟click事件
+    expect(fn).toBeCalled() //期待被调用
   })
 })
