df38a96c15f18d44cf96020bb48631550300d56e
st <st@lqkj.com>
2021-11-26 18:03:49 +0800

    发布包到npm

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-import Icon from './icon/icon'   
+// import Icon from './icon/icon'   
 
-const fn: React.MouseEventHandler = (e)=>{
-    console.log(e.target)
-}
+// const fn: React.MouseEventHandler = (e)=>{
+//     console.log(e.target)
+// }
 
-ReactDOM.render(
-    <div>
-        <Icon className='qqq' name='alipay' onClick={fn} onMouseEnter={()=>{console.log(123)}} onMouseLeave={()=>{console.log(345)}}/>
-        {/* <Icon name='qq'/>
-        <Icon name='wechat'/> */}
-    </div>
-    ,document.getElementById('root'))
+// ReactDOM.render(
+//     <div>
+//         <Icon className='qqq' name='alipay' onClick={fn} onMouseEnter={()=>{console.log(123)}} onMouseLeave={()=>{console.log(345)}}/>
+//     </div>
+//     ,document.getElementById('root'))
+
+export {default as Icon} from './icon/icon' //引入并且导出
