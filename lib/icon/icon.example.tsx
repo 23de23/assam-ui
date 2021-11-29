ddb8087e7308e63c0d15ca358d38d277487b5c68
23de23 <1414371604@qq.com>
2021-11-29 23:18:44 +0800

    搭建文档：路由

---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Icon from './icon'
+
+ const IconExample:React.FunctionComponent = ()=>{
+    return(
+        <div>
+            <Icon name='qq'/>
+            <Icon name='alipay'/>
+            <Icon name='wechat'/>
+        </div>
+    )
+}
+
+export default IconExample
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,10 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports =Object.assign({},base,{
   mode: 'development',
+  entry: { // 配置入口处
+    index: './example.tsx'
+  },
   plugins: [ //插件
       new HtmlWebpackPlugin({  //使用Html-webpack-plugin插件使根目录下的index.html变为weback的首页，并且自动引入入口文件（js、tsx）
           title: 'frankUI', //见index.html中title处
-          template: 'index.html'
+          template: 'example.html'
       })
   ],
 }) 
