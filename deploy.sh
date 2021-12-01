87ac92f353848c4bfb60518166fcdcc69599e3ef
st <st@lqkj.com>
2021-12-01 10:41:22 +0800

    upload

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/env bash   
 # 使用环境变量中bash运行
 npm version $1 && \
-  git commit . -m "deploy" && \
-  git push origin master:deploy
+  git commit . -m "deploy"
 # $1 = arguments[0]
 # \换行 但是是一行语句执行
 # &&同js强断语句
