809ba8872dd95cb500d388196d40839cbf710344
st <st@lqkj.com>
2021-12-01 10:31:00 +0800

    自动化部署代码修改

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash   
 # 使用环境变量中bash运行
 npm version $1 && \
-  git push --follow-tags
+  git commit . -m "deploy"
+  git push origin master:deploy
 # $1 = arguments[0]
 # \换行 但是是一行语句执行
 # &&同js强断语句
