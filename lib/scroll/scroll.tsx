414c18bcb3e2f401ec37097231fef7016e9882fc
23de23 <1414371604@qq.com>
2021-12-15 21:55:48 +0800

    完成滚动条基础功能

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import { scopedClassMaker } from "../helpers/classes"
 
@@ -14,10 +14,32 @@ const sc = scopedClassMaker('assam-scroll')
 
 const Scroll = (props:Props) => {
   const {children,className,...restProps}  = props
+
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [scrollBarHeight,setScrollBarHeight] = useState(0)
+  const [scrollBarTop,setScrollBarTop] = useState(0)
+  useEffect(()=>{
+    const {current} = containerRef
+    const viewHeight = current!.getBoundingClientRect().height //可视区域高度
+    const scrollHeight = current!.scrollHeight //可滚动区域高度
+    setScrollBarHeight(viewHeight*viewHeight/scrollHeight)
+  },[])
+  const onScroll = () => {
+    const {current} = containerRef
+    const viewHeight = current!.getBoundingClientRect().height //可视区域高度
+    const scrollHeight = current!.scrollHeight //可滚动区域高度
+    const scrollTop = current!.scrollTop
+    setScrollBarTop(scrollTop*viewHeight/scrollHeight)
+  }
+
   return(
     <div {...restProps} className={sc('',{extra:className})}>
-      <div className={sc('inner')} style={{right:-scrollbarWidth()}}>
+      <div className={sc('inner')} onScroll={onScroll} ref={containerRef} style={{right:-scrollbarWidth()}}>
         {children}
+      </div>
+      <div className={sc('track')}>
+        <div style={{height:scrollBarHeight,transform:`translateY(${scrollBarTop}px)`}} className={sc('scrollBar')}></div>
       </div>
     </div>
   )
